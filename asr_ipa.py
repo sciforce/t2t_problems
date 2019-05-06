ffac997d9a8295933aec2bffefd6575e57ba0d7b
Eugene Karaulov <ekaraulov@sciforce.solutions>
2019-05-06 20:09:31 +0300

    Adding encoder for arbitrary corpus with annotations in csv format.

--- asr_ipa.py
+++ asr_ipa.py
@@ -125,6 +125,8 @@ class AsrIpa(speech_recognition.SpeechRecognitionProblem):
         vocab_path = os.path.join(model_hparams.data_dir, VOCAB_FILENAME)
         with tf.gfile.Open(vocab_path) as fid:
             vocab = fid.read().strip().split('\n')
+        p.modality = {"inputs": modalities.ModalityType.SPEECH_RECOGNITION,
+                      "targets": modalities.ModalityType.CLASS_LABEL}
         model_hparams.vocab_size = {"inputs": None,
                                     "targets": len(vocab)}
         tf.logging.info('Setting vocabulary size to %d',
